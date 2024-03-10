1658765ad53a490b5bd3db2ce617bd17f69ebb9f
Nassim <jslegend@protonmail.com>
2024-03-09 22:21:50 -0500

    fix

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,7 @@
 import { defineConfig } from "vite";
 
 export default defineConfig({
-  base: "/2d-portfolio-kaboom/",
+  base: "https://github.com/JSLegendDev/2d-portfolio-kaboom/",
   build: {
     minify: "terser",
   },
