8103d38a48dd5edd27148efd6c75aaa4984fcdd4
Natalia Tamara  Rosa <109100346+Natamrosa6@users.noreply.github.com>
2024-08-01 17:52:09 +0200

    carpeta vacia

--- src/kaboomCtx.js
+++
@@ -1,9 +0,0 @@
-import kaboom from "kaboom";
-import { scaleFactor } from "./constants";
-
-export const k = kaboom({
-  global: false,
-  touchToMouse: true,
-  canvas: document.getElementById("game"),
-  debug: false, // set to false once ready for production
-});
--- vite.config.js
+++
@@ -1,8 +0,0 @@
-import { defineConfig } from "vite";
-
-export default defineConfig({
-  base: "./",
-  build: {
-    minify: "terser",
-  },
-});
